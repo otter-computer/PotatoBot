76c947bcaf8d53d9276c155d7a14e850ea2656c8
Glitch (artistic-thundering-sweatshirt) <none>
2021-01-13 10:03:40 +0000

    🎲👨 Checkpoint ./package.json:27179669/713 ./index.js:27179669/299 ./index.html:27179669/799

--- index.js
+++ index.js
@@ -35,3 +35,14 @@ bot.on('message', Message => {
 });
 
 bot.login(process.env.AUTH_TOKEN);
+
+// Serve index.html when visiting this site
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'content-type': 'text/html' });
+  fs.createReadStream('index.html').pipe(res);
+});
+
+server.listen(process.env.PORT || 3000);
