2c2d249cc791391068f2f178b4e162ee39b2e7b3
Glitch (artistic-thundering-sweatshirt) <none>
2021-01-13 09:51:59 +0000

    🏋🔦 Checkpoint ./index.js:27179669/649 ./package.json:27179669/812

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ function cleanup() {
 process.on('SIGINT', cleanup);
 process.on('SIGTERM', cleanup);
 
-// Message Handler
-function handleMessage(Message) {
+// Events
+bot.on('ready', () => {
+  console.log('Bot connected.');
+});
 
+bot.on('message', Message => {
   // Ignore Discord system message
   if (Message.system) return;
 
@@ -28,19 +31,6 @@ function handleMessage(Message) {
   // Reply
   if (Message.mentions.users.has(bot.user.id)) {
     Message.reply(':potato:');
-  }
-}
-
-// Events
-bot.on('ready', () => {
-  console.log('Bot connected.');
-});
-
-bot.on('message', Message => {
-  try {
-    handleMessage(Message);
-  } catch (e) {
-    console.error(e.stack);
   }
 });
 
